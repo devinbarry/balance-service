a12a2d33a958a0a40e7f595903dc20397f10b3b3
Devin Barry <devinbarry@users.noreply.github.com>
2022-11-02 23:18:31 +0000

    Show the Transaction model in the Django admin.

--- balance/api/admin.py
+++ balance/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_filter = ('currency', )
+    readonly_fields = ('created_date', 'modified_date', 'user', 'amount', 'currency', 'type', 'data')
+    list_display = ('__str__', 'created_date', 'currency', 'type', 'amount')
+    # actions = ['create_deposit']
