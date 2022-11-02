d7870c219c9d4e42d82bbff5f0c0dbb31a8296f9
Devin Barry <devinbarry@users.noreply.github.com>
2022-11-02 23:18:31 +0000

    Create a management command that creates example data in the database.

---
+++ balance/api/management/commands/create_data.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth.models import User
+from ...models import Transaction, create_transfer
+
+
+class Command(BaseCommand):
+    """
+    Setup example data for the API
+    """
+
+    help = 'Generate example data for the API.'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Creating example data...')
+        self._create_transactions()
+
+    def _create_transactions(self):
+        """
+        Create example transactions.
+        """
+        foo, bar = self._create_users()
+        Transaction.create_deposit(user=foo, amount=10000, currency='EUR')
+        Transaction.create_deposit(user=bar, amount=15000, currency='EUR')
+        create_transfer(user_from=foo, user_to=bar, amount=5000, currency='EUR')
+        Transaction.create_withdrawal(user=bar, amount=10000, currency='EUR')
+        self.stdout.write(f'Created example transactions')
+
+    def _create_users(self):
+        """
+        Create example users
+        """
+        foo, created = User.objects.get_or_create(username='foo')
+        if created:
+            self.stdout.write(f'Created new user: {foo}')
+        bar, created = User.objects.get_or_create(username='bar')
+        if created:
+            self.stdout.write(f'Created new user: {bar}')
+        return foo, bar
