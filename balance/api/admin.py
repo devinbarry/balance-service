df52d3299ce7078a6df024e11c85c6f229519468
Devin Barry <devinbarry@users.noreply.github.com>
2022-11-03 23:57:45 +0000

    Show the users balance in the Django admin.

--- balance/api/admin.py
+++ balance/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
+from .balance_utils import get_user_balance
 from .models import Transaction
 
 
@@ -9,3 +12,16 @@ class TransactionAdmin(admin.ModelAdmin):
     readonly_fields = ('created_date', 'modified_date')
     list_display = ('__str__', 'created_date', 'currency', 'type', 'amount')
     # actions = ['create_deposit']
+
+
+class BalanceUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'balance')
+    list_filter = ('is_staff', 'is_superuser')
+    search_fields = ('username', )
+
+    def balance(self, obj):
+        return get_user_balance(obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, BalanceUserAdmin)
