79aae1b41b9d9fdaacc596ed347e0972b98a4122
Devin Barry <devinbarry@users.noreply.github.com>
2022-11-04 00:04:27 +0000

    Add a very simple API view for exposing user balances.

---
+++ balance/api/urls.py
@@ -0,0 +1,16 @@
+from rest_framework import routers
+
+from django.urls import include, path
+
+from .views import BalanceView
+
+# TODO expose Transactions through this router
+router = routers.DefaultRouter()
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('balances/', BalanceView.as_view(), name='balances'),
+]
--- balance/api/views.py
+++ balance/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import permissions, authentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from .balance_utils import get_user_balance
+
+
+class BalanceView(APIView):
+    """
+    Get user balance
+    """
+    permission_classes = [permissions.IsAdminUser]
+    authentication_classes = [authentication.SessionAuthentication]
+
+    def get(self, request, format=None):
+        """
+        Return a balance for a specific user.
+        """
+        username = request.GET.get('username')
+        user = get_object_or_404(User, username=username)
+        return Response(get_user_balance(user))
